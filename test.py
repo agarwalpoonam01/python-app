7a226409e09803c7f68d28b4bc734b7eb2822216
hellodk <hello.dk@outlook.com>
2020-06-09 22:04:23 +0530

    Added Jenkinsfile

--- test.py
+++ test.py
@@ -5,9 +5,10 @@ import requests
 # def test_default_greeting_set(self):
 #         self.assertEqual(hello_name(poonam), 'Hello poonam!')
 
-def test_get_locations_for_us_90210_check_status_code_equals_200():
-	response = requests.get("http://127.0.0.1:5000/hello/poonam")
-	assert response.status_code == 200
 
-# if __name__ == '__main__': 
-# 	unittest.main()	
+def test_get_locations_for_us_90210_check_status_code_equals_200():
+    response = requests.get("http://127.0.0.1:5000/hello/poonam")
+    assert response.status_code == 200
+
+# if __name__ == '__main__':
+# 	unittest.main()
